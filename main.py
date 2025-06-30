f663f851c80af617899ff3f64441c5807087970c
HVEngineer <justin.fleming09@gmail.com>
2025-06-30 18:53:37 -0400

    completed bookbot

--- main.py
+++ main.py
@@ -2,17 +2,28 @@ from stats import count_words #from name of file without .py import function to 
 from stats import count_chars
 from stats import lower_convert
 from stats import string_split
+from stats import dict_sort
+import sys
 
 def get_book_text (filepath): #opens and reads file
     with open(filepath) as f:    
         return f.read()
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt") #calls and passes file directory
-    count_words(book_text) #not needed at this time
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    filepath = sys.argv[1]
+    print('===============BOOKBOT==================')
+    print(f'Analyzing book found at {filepath}')
+    print('------------ Word Count -------------')
+    book_text = get_book_text(filepath) #calls and passes file directory
+    count_words(book_text) # counts words in the text
     lower_case = lower_convert(book_text) #convert all characters to lowercase
     split_string = string_split(lower_case) #split words into individual strings
-    count_chars(split_string) #count characters in each word string, incrementing counter
+    counted_chars = count_chars(split_string) #count characters in each word string, incrementing counter
+    print('----------- Character Count -----------')
+    dict_sort(counted_chars) #turns dictionary into list and reverse sorts by value
     
 
 main()
--- stats.py
+++ stats.py
@@ -1,15 +1,15 @@
 def count_words(text): #counts how many words in book
     split_text = text.split()
     num_words = len(split_text)
-    print (num_words, "words found in the document")
+    print (f'Found {num_words} total words')
 
 def lower_convert(text): #sets all characters to lowercase
     return str.lower(text)
 
-def string_split(text):
+def string_split(text): #splits text into individual strings
     return text.split()
 
-def count_chars(text):
+def count_chars(text): #counts individual characters, adds them to dictionary
     char_dict = {}
     for words in text:
         for char in words:
@@ -17,8 +17,20 @@ def count_chars(text):
                 char_dict[char] = 1
             else:
                 char_dict[char] += 1
-    print (char_dict)
+    return char_dict
 
+def dict_sort(dict_to_sort):
+    dict_list = []
+    for key, value in dict_to_sort.items():
+        dict_list.append({"char":key, "num": value})
+        dict_list.sort(reverse = True ,key=lambda item: item["num"])
+    
+    for item in dict_list:
+        if item['char'].isalpha():
+            print(f"{item['char']}: {item['num']}")
+
+        #.sort(reverse=True, key=sort_on)
+    #print(sorted_dict)
 
 
 
