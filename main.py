4af0206bd2e202c96f0661cc5202dbd5acc3ee17
HVEngineer <justin.fleming09@gmail.com>
2025-06-29 23:54:53 -0400

    added ability to parse and count individual characters

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-def get_book_text (filepath):
-    #print("in get_book_text")
+from stats import count_words #from name of file without .py import function to import
+from stats import count_chars
+from stats import lower_convert
+from stats import string_split
+
+def get_book_text (filepath): #opens and reads file
     with open(filepath) as f:    
         return f.read()
 
-from stats import count_words #from name of file without .py import function to import
-
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
-    count_words(book_text)
+    book_text = get_book_text("books/frankenstein.txt") #calls and passes file directory
+    count_words(book_text) #not needed at this time
+    lower_case = lower_convert(book_text) #convert all characters to lowercase
+    split_string = string_split(lower_case) #split words into individual strings
+    count_chars(split_string) #count characters in each word string, incrementing counter
     
 
 main()
--- stats.py
+++ stats.py
@@ -1,5 +1,26 @@
-def count_words(book_text):
-    #print("in count_words")
-    split_text = book_text.split()
+def count_words(text): #counts how many words in book
+    split_text = text.split()
     num_words = len(split_text)
     print (num_words, "words found in the document")
+
+def lower_convert(text): #sets all characters to lowercase
+    return str.lower(text)
+
+def string_split(text):
+    return text.split()
+
+def count_chars(text):
+    char_dict = {}
+    for words in text:
+        for char in words:
+            if char not in char_dict:
+                char_dict[char] = 1
+            else:
+                char_dict[char] += 1
+    print (char_dict)
+
+
+
+
+        
+        
